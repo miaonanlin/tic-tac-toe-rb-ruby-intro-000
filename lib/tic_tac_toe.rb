17550c0f71f9bff9e2ff030f5766c1072b6cfb5a
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-27 18:57:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,3 @@
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -17,21 +6,35 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+WIN_COMBINATIONS = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [6, 4, 2]
+]
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player)
+
+def move(board, index, current_player )
   board[index] = current_player
 end
 
-def position_taken?(board, index)
+def postion_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
+
 
 def turn_count(board)
   turn = 0
@@ -54,18 +57,6 @@ def current_player(board)
   return player
 end
 
-def turn(board)
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    player_token = current_player(board)
-    move(board, index, player_token)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
@@ -84,6 +75,7 @@ def won?(board)
     end
   }
   return false
+  end
 end
 
 def full?(board)
@@ -91,19 +83,23 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else
-    return false
-  end
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board)
-    return true
-  else
-    return false
+  won?(board) || draw?(board)
+end
+
+
+def turn(board)
+  puts "Please enter number 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if !valid_move?(board, index)
+    turn(board)
   end
+  move(board, index, current_player )
+  display_board(board)
 end
 
 def winner (board)
